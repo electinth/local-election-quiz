7166694305644d5ffb88bbec20db35c7fbf7135d
Th1nkK1D <witheep@gmail.com>
2020-11-13 11:59:05 +0700

    Add lottie player

---
+++ src/components/lottie-player.vue
@@ -0,0 +1,38 @@
+<template>
+  <div ref="container" class="w-full h-full"></div>
+</template>
+
+<script>
+import { defineComponent, ref, onMounted } from 'vue';
+import lottie, { AnimationConfigWithData } from 'lottie-web';
+
+export default defineComponent({
+  props: {
+    animationData: {
+      type: Object,
+      required: true
+    },
+    config: {
+      type: AnimationConfigWithData,
+      default: {}
+    }
+  },
+  setup(props) {
+    const container = ref(null);
+
+    onMounted(() => {
+      lottie.loadAnimation({
+        animationData: props.animationData,
+        container: container.value,
+        autoplay: true,
+        loop: true,
+        ...props.config
+      });
+    });
+
+    return {
+      container
+    };
+  }
+});
+</script>
--- src/views/home.vue
+++ src/views/home.vue
@@ -1,5 +1,22 @@
 <template>
   <div>
-    Landing
+    <LottiePlayer :animationData="stepAnimation" />
   </div>
 </template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import LottiePlayer from '../components/lottie-player.vue';
+import stepAnimation from '../assets/lotties/step.json';
+
+export default defineComponent({
+  components: {
+    LottiePlayer
+  },
+  setup() {
+    return {
+      stepAnimation
+    };
+  }
+});
+</script>
