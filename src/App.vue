883a0f3b5aa63bcee278180979b78c029890ffed
Th1nkK1D <witheep@gmail.com>
2020-11-13 11:16:55 +0700

    Add header bar

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,19 @@
 <template>
-  <H1>I'm Vue 3</H1>
-  <H2>I'm Vue 3</H2>
-  <H3>I'm Vue 3</H3>
-  <P class="text-green">Test typography</P>
+  <div class="w-screen min-h-screen flex flex-col bg-gray">
+    <div class="flex flex-row px-4 py-2">
+      <img
+        src="./assets/images/header-logo.png"
+        alt="ELECT x King Prajadhipok's Insitute"
+        class="h-8"
+      />
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 
-import H1 from './components/typography/h1.vue';
-import H2 from './components/typography/h2.vue';
-import H3 from './components/typography/h3.vue';
-import P from './components/typography/p.vue';
-
 export default defineComponent({
-  name: 'App',
-  components: {
-    H1,
-    H2,
-    H3,
-    P
-  }
+  name: 'App'
 });
 </script>
