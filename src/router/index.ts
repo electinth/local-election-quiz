6b8f00843b1182bf4750f70af356ca88f909a56a
Th1nkK1D <witheep@gmail.com>
2020-11-26 11:28:37 +0700

    Add userid validation route guard

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,13 @@
-import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router';
+import {
+  createRouter,
+  createWebHashHistory,
+  NavigationGuardNext,
+  RouteLocationNormalized,
+  RouteRecordRaw
+} from 'vue-router';
+
 import Home from '../views/home.vue';
+import { ProviderName } from '@/constants/provider';
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -26,9 +34,27 @@ const routes: Array<RouteRecordRaw> = [
   }
 ];
 
+const redirectToHomeIfUserIdIsNotGenerated = (
+  { name }: RouteLocationNormalized,
+  from: RouteLocationNormalized,
+  next: NavigationGuardNext
+) => {
+  if (
+    name !== 'Home' &&
+    name !== 'Quiz' &&
+    !sessionStorage.getItem(ProviderName.UserId)
+  ) {
+    next({ name: 'Home', replace: true });
+  } else {
+    next();
+  }
+};
+
 const router = createRouter({
   history: createWebHashHistory(),
   routes
 });
+
+router.beforeEach(redirectToHomeIfUserIdIsNotGenerated);
 
 export default router;
